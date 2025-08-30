9d2214dfbbfac4fd40f13e08ebe4812c71ec457a
unlimitedcodeG <1659159807@qq.com>
2025-08-31 02:13:02 +0800

    l:chan

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	ch := make(chan int, 1)
+	select {
+	case ch <- 1:
+		fmt.Println("send success")
+	default:
+		fmt.Println("send failed")
+	}
 }
-	
