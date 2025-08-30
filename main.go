7364e09887f8d92e0650c3676377b84404391f63
unlimitedcodeG <1659159807@qq.com>
2025-08-31 02:42:53 +0800

    string A minus string a =ASCII

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ func f() {}
 var g = "g"
 
 func main() {
-	f := "f"
-	fmt.Println(f) // "f"; local var f shadows package-level func f
-	fmt.Println(g) // "g"; package-level var
-	// fmt.Println(h) // compile error: undefined: h
+	x := "hello"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != ' ' {
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+		}
+	}
+
 }
